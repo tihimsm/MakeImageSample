a7339f78b03d6b4900d296b2857d360238d12234
tihimsm <tihimsm@gmail.com>
2020-10-27 12:12:02 +0900

    Initial Commit

---
+++ MakeImageSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MakeImageSample
+//
+//  Created by Taihei Mishima on 2020/10/27.
+//  Copyright © 2020 Taihei Mishima. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
